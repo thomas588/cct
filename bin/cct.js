ef50e6dde58847a10aa359a94d1e1fd79f97f439
ali_umurzakov24 <thomasthompson0204@gmail.com>
2026-02-01 23:12:15 +0500

    Add --version flag

--- bin/cct.js
+++ bin/cct.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { init } = require('../lib/init');
+const { version } = require('../package.json');
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -8,13 +9,16 @@ const command = args[0];
 if (command === 'init') {
   const name = args[1] || '.';
   init(name);
+} else if (command === '--version' || command === '-v') {
+  console.log(`cct v${version}`);
 } else if (command === '--help' || command === '-h' || !command) {
   console.log(`
-CCT - Claude Code Team
+CCT - Claude Code Team v${version}
 
 Usage:
   cct init <name>     Create new CCT project
   cct init .          Initialize in current folder
+  cct --version       Show version
   cct --help          Show this help
 
 Example:
